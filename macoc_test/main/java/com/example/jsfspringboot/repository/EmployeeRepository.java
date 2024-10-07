0bbf3931df0c98466ed934ddd19549c311281161
dsolonina <dsolonina@gmail.com>
2024-10-07 12:00:31 +0300

    test Emploee 123

--- macoc_test/main/java/com/example/jsfspringboot/repository/EmployeeRepository.java
+++ macoc_test/main/java/com/example/jsfspringboot/repository/EmployeeRepository.java
@@ -7,6 +7,6 @@ import java.util.List;
 
 public interface EmployeeRepository extends CrudRepository<EmployeeEntity, Integer> {
 
-    List<EmployeeEntity> findAll();
+    List<EmployeeEntity> findAllTest();
 
 }

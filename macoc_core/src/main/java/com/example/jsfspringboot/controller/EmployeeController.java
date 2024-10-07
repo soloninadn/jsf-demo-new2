dea5a96a73173348d55a60444ad44f21c1b8dad7
dsolonina <dsolonina@gmail.com>
2024-10-07 12:27:52 +0300

    test controller 123

--- macoc_core/src/main/java/com/example/jsfspringboot/controller/EmployeeController.java
+++ macoc_core/src/main/java/com/example/jsfspringboot/controller/EmployeeController.java
@@ -23,6 +23,6 @@ public class EmployeeController {
 
     public String saveEmployee(EmployeeBean employeeBean) {
         employeeService.save(employeeBean);
-        return "index.xhtml";
+        return "index123.xhtml";
     }
 }
